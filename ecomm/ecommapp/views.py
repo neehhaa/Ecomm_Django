ebe47cb3a82bd04ad6c61f10ab0e8e48ea1a9ea2
neehhaa <nehawasnik95@outlook.com>
2021-09-30 22:12:47 +0000

    addition of number

--- ecomm/ecommapp/views.py
+++ ecomm/ecommapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def add (request):
+def home(request):
+    return render(request,'home.html',{'name':'Neha'})
+def add(request):
     val1 = int(request.GET['num1'])
     val2 = int(request.GET['num2'])
-    res= val1+val2
-    return render(request,'result.html',{'result':'res'})
+    res = val1+val2
+    return render(request,'result.html',{'result':res})
